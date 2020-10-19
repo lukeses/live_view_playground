5cd22d4cfbd6cc2cd63301a8519f288ab51fa5c5
Łukasz Jankowski <lukaszpjankowski@gmail.com>
2020-10-19 22:31:38 +0200

    playing with pagination

--- lib/demo_web/live/users_live.ex
+++ lib/demo_web/live/users_live.ex
@@ -1,10 +1,7 @@
 defmodule DemoWeb.UsersLive do
   use DemoWeb, :live_view
 
-  def mount(_params, %{}, socket) do
-    uuid = Ecto.UUID.generate
-    {:ok, assign(socket, uuid: uuid)}
-  end
+  @cats %{1 => ["Jack", "John"], 2 => ["Tom", "Jerry"], 3 => ["Luke", "Tom"]}
 
   def handle_event("show_next", _value, socket) do
     uuid = Ecto.UUID.generate
@@ -12,4 +9,10 @@ defmodule DemoWeb.UsersLive do
     {:noreply, assign(socket, :uuid, uuid)}
   end
 
+  def handle_params(%{"page" => page}, _, socket) do
+    {page, _} = Integer.parse(page)
+    cats_on_page = Map.get(@cats, page)
+
+    {:noreply, assign(socket, cats: cats_on_page, page: page)}
+  end
 end
